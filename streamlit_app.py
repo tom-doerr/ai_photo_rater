ce2b032f554e3bac818a11337ab4a1e9aea6f5eb
Tom Dörr <tom.doerr@tum.de>
2022-06-12 05:06:13 +0200

    Add support for multiple images

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,48 +43,16 @@ st.sidebar.write(f'Earliest page load: {get_earliest_page_load_time()}')
 os.makedirs(IMAGES_FOLDER, exist_ok=True)
 
 # photo_file = st.file_uploader("Upload a photo", type=["jpg", "png"])
-photo_file = st.file_uploader("Upload a photo")
+photo_files = st.file_uploader("Upload a photo", accept_multiple_files=True)
 
-if not photo_file:
+if not photo_files:
     st.write("Please upload a photo")
     st.stop()
-
-
-col1, col2, col3 = st.columns([10,10,10])
-with st.spinner('Loading...'):
-    with col2:
-        st.image(photo_file, use_column_width=True)
-
-
-# save it
-filename = f'{time.time()}'.replace('.', '_')
-filename_path = f'{IMAGES_FOLDER}/{filename}'
-with open(f'{filename_path}', 'wb') as f:
-    f.write(photo_file.read())
 
 
 
 c = Client('grpcs://demo-cas.jina.ai:2096')
 
-# r = c.rank(
-    # [
-        # Document(
-            # # uri='https://www.pngall.com/wp-content/uploads/12/Britney-Spears-PNG-Image-File.png',
-            # # uri='britney2.png',
-            # uri=filename_path,
-            # matches=[
-                # Document(text=f'the person is {p}')
-                # for p in (
-                        # 'pretty',
-                        # 'ugly',
-                # )
-            # ],
-        # )
-    # ]
-# )
-
-# text_and_scores = r['@m', ['text', 'scores__clip_score__value']]
-# st.write(text_and_scores)
 
 
 def rate_image(image_path, target, opposite):
@@ -124,37 +92,42 @@ def create_analysis(filename, target, opposite, label):
 
 
 
-# score = rate_image(filename_path, 'the person is pretty', 'the person is ugly')
-
-if False:
+def process_image(photo_file):
     col1, col2, col3 = st.columns([10,10,10])
-    with col1:
-        create_analysis(filename_path, 'the person is pretty', 'the person is ugly', 'Beauty')
-
-    with col2:
-        create_analysis(filename_path, 'the person is trustworthy', 'the person is dishonest', 'Trustworthiness')
-
-    with col3:
-        create_analysis(filename_path, 'the person is smart', 'the person is stupid', 'Intelligence')
+    with st.spinner('Loading...'):
+        with col2:
+            st.image(photo_file, use_column_width=True)
 
 
-
-
-with st.spinner('Rating your photo...'):
-    score_beauty = rate_image(filename_path, 'the person is pretty', 'the person is ugly')
-    score_trustworthiness = rate_image(filename_path, 'the person is trustworthy', 'the person is dishonest')
-    score_intelligence = rate_image(filename_path, 'the person is smart', 'the person is stupid')
-
-
-    # plot them
-    import plotly.graph_objects as go
-
-
-    fig = go.Figure(data=[go.Bar(x=['Beauty', 'Trustworthiness', 'Intelligence'], y=[score_beauty*100, score_trustworthiness*100, score_intelligence*100])], layout=go.Layout(title='Scores'))
-    # range 0 to 100 for the y axis:
-    fig.update_layout(yaxis=dict(range=[0, 100]))
-
-    st.plotly_chart(fig, use_container_width=True)
+    # save it
+    filename = f'{time.time()}'.replace('.', '_')
+    filename_path = f'{IMAGES_FOLDER}/{filename}'
+    with open(f'{filename_path}', 'wb') as f:
+        f.write(photo_file.read())
 
 
 
+
+
+
+    with st.spinner('Rating your photo...'):
+        score_beauty = rate_image(filename_path, 'the person is pretty', 'the person is ugly')
+        score_trustworthiness = rate_image(filename_path, 'the person is trustworthy', 'the person is dishonest')
+        score_intelligence = rate_image(filename_path, 'the person is smart', 'the person is stupid')
+
+
+        # plot them
+        import plotly.graph_objects as go
+
+
+        fig = go.Figure(data=[go.Bar(x=['Beauty', 'Trustworthiness', 'Intelligence'], y=[score_beauty*100, score_trustworthiness*100, score_intelligence*100])], layout=go.Layout(title='Scores'))
+        # range 0 to 100 for the y axis:
+        fig.update_layout(yaxis=dict(range=[0, 100]))
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
+
+for photo_file in photo_files:
+    process_image(photo_file)
+    st.markdown('---')
