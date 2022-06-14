17615cd80838e019d88acd1408a75f9eef4f40eb
Tom Dörr <tom.doerr@tum.de>
2022-06-14 04:07:38 +0200

    Update metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,14 @@ import os
 
 IMAGES_FOLDER = 'images'
 PAGE_LOAD_LOG_FILE = 'page_load_log.txt'
+METRIC_TEXTS = {
+    'Attractivness': ('the person is attractive', 'the person is unattractive'),
+    'Hotness': ('the person is hot', 'the person is ugly'),
+    'Trustworthiness': ('the person is trustworthy', 'the person is dishonest'),
+    'Intelligence': ('the person is smart', 'the person is stupid'),
+}
 
-st.set_page_config(page_title='AI Photo Rater', initial_sidebar_state="collapsed")
+st.set_page_config(page_title='AI Photo Rater', initial_sidebar_state="auto")
 
 
 st.title('AI Photo Rater')
@@ -33,13 +39,51 @@ def get_earliest_page_load_time():
 
 
 
+def show_sidebar_metrics():
+    metric_options = ['Attractivness', 'Hotness', 'Trustworthiness', 'Intelligence']
+    default_metrics = ['Attractivness', 'Trustworthiness', 'Intelligence'] 
+    st.sidebar.title('Metrics')
+    # metric = st.sidebar.selectbox('Select a metric', metric_options)
+    selected_metrics = []
+    for metric in metric_options:
+        selected = metric in default_metrics
+        if st.sidebar.checkbox(metric, selected):
+            selected_metrics.append(metric)
+
+    with st.sidebar.expander('Metric texts'):
+        st.write(METRIC_TEXTS)
+
+    print("selected_metrics:", selected_metrics)
+    return selected_metrics
+
+
+def get_custom_metric():
+    st.sidebar.markdown('**Custom metric**:')
+    metric_name = st.sidebar.text_input('Metric name', placeholder='e.g. "Youth"')
+    metric_target = st.sidebar.text_input('Metric target', placeholder='this person is young')
+    metric_opposite = st.sidebar.text_input('Metric opposite', placeholder='this person is old')
+    return {metric_name: (metric_target, metric_opposite)}
 
 
 
 
 log_page_load()
+
+metrics = show_sidebar_metrics()
+st.sidebar.markdown('---')
+custom_metric = get_custom_metric()
+st.sidebar.markdown('---')
 st.sidebar.write(f'Page loads: {get_num_page_loads()}')
 st.sidebar.write(f'Earliest page load: {get_earliest_page_load_time()}')
+
+metric_texts = METRIC_TEXTS
+print("custom_metric:", custom_metric)
+custom_key = list(custom_metric.keys())[0]
+if custom_key:
+    custom_tuple = custom_metric[custom_key]
+    if custom_tuple[0] and custom_tuple[1]:
+        metrics.append(list(custom_metric.keys())[0])
+        metric_texts = {**metric_texts, **custom_metric}
 
 os.makedirs(IMAGES_FOLDER, exist_ok=True)
 
@@ -49,7 +93,6 @@ photo_files = st.file_uploader("Upload a photo", accept_multiple_files=True)
 photo_files = sorted(photo_files, key=lambda x: x.name)
 
 if not photo_files:
-    st.write("Please upload a photo")
     st.stop()
 
 
@@ -84,7 +127,7 @@ def rate_image(image_path, target, opposite, attempt=0):
 
 
 # @st.cache
-def process_image(photo_file):
+def process_image(photo_file, metrics):
     col1, col2, col3 = st.columns([10,10,10])
     with st.spinner('Loading...'):
         with col1:
@@ -105,29 +148,30 @@ def process_image(photo_file):
 
 
 
-
     with st.spinner('Rating your photo...'):
-        score_beauty = rate_image(filename_path, 'the person is attractive', 'the person is unattractive')
-        score_trustworthiness = rate_image(filename_path, 'the person is trustworthy', 'the person is dishonest')
-        score_intelligence = rate_image(filename_path, 'the person is smart', 'the person is stupid')
+        scores = dict()
+        for metric in metrics:
+            target = metric_texts[metric][0]
+            opposite = metric_texts[metric][1]
+            score = rate_image(filename_path, target, opposite)
+            scores[metric] = score
 
+
+        scores['Avg'] = sum(scores.values()) / len(scores)
 
         # plot them
         import plotly.graph_objects as go
 
 
-        fig = go.Figure(data=[go.Bar(x=['Beauty', 'Trustworthiness', 'Intelligence'], y=[score_beauty*100, score_trustworthiness*100, score_intelligence*100])], layout=go.Layout(title='Scores'))
+        scores_percent = []
+        for metric in metrics:
+            scores_percent.append(scores[metric] * 100)
+        fig = go.Figure(data=[go.Bar(x=metrics, y=scores_percent)], layout=go.Layout(title='Scores'))
         # range 0 to 100 for the y axis:
         fig.update_layout(yaxis=dict(range=[0, 100]))
 
         st.plotly_chart(fig, use_container_width=True)
 
-    scores = {
-        'beauty': score_beauty,
-        'trustworthiness': score_trustworthiness,
-        'intelligence': score_intelligence,
-        'avg': (score_beauty + score_trustworthiness + score_intelligence) / 3
-    }
     return filename_path, scores
 
 
@@ -146,7 +190,7 @@ def get_best_image(image_scores_list, metric):
 image_scores_list = []
 for photo_file in photo_files:
     # process_image(photo_file)
-    filename_path, scores = process_image(photo_file)
+    filename_path, scores = process_image(photo_file, metrics)
     # image_scores_list.append((filename_path, scores))
     image_scores_list.append((photo_file, scores))
     st.markdown('---')
@@ -154,12 +198,12 @@ for photo_file in photo_files:
 
 if len(photo_files) > 1:
     st.title('Best image')
-    metric = st.selectbox('Select a metric', ['avg', 'beauty', 'trustworthiness', 'intelligence'])
+    metric = st.selectbox('Select a metric', ['Avg'] + metrics)
     image_file = get_best_image(image_scores_list, metric)
     # st.image(image_file, use_column_width=True)
     # from PIL import Image
     # image_file = Image.open(image_path)
-    process_image(image_file)
+    process_image(image_file, metrics)
 
 
 st.markdown('---')
